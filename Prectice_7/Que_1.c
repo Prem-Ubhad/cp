33574eeb66c61f6b37c3040df1352b280e1c319c
Prem-Ubhad <coderprem0@gmail.com>
2023-03-31 21:55:09 +0530

    cp

--- Prectice_7/Que_1.c
+++ Prectice_7/Que_1.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
 int main(){
-    
+    int marks[5];
+   for (int i = 0; i < 5; i++)
+   {
+    printf("Enter the marks of student %d : ",i+1);
+    scanf("%d",&marks[i]);
+   }
+   
+    printf("The Marks of students entered are %d %d %d %d and %d",marks[0],marks[1],marks[2],marks[3],marks[4]);
     return 0;
 }
---
+++ Prectice_7/Que_2.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+    float marks[] = {30.56,45.95,99.25,46.96};
+    printf("The value of marks[0] is : %.2f\n",marks[0]);
+    printf("The value of marks[1] is : %.2f\n",marks[1]);
+    printf("The value of marks[2] is : %.2f\n",marks[2]);
+    printf("The value of marks[3] is : %.2f\n",marks[3]);
+    return 0;
+}
